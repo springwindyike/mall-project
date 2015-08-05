687b160d2166d0afaf32b52f0e2b5e0be13ef9a0
殷林 <yinlin@91taoci.cn>
2015-08-05 22:13:27 +0800

    Title:解决返回json Content:解决返回Json

--- mall-api/src/main/java/com/ishare/mall/controller/IndexController.java
+++ mall-api/src/main/java/com/ishare/mall/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.ishare.mall.controller;
 
+import com.ishare.mall.core.model.product.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,9 +14,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/index")
 public class IndexController {
-    @RequestMapping(value="get",method = RequestMethod.GET)
+    @RequestMapping(value="get", method = RequestMethod.GET)
     @ResponseBody
     public String get() {
        return "success";
+    }
+
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    @ResponseBody
+    public Product list() {
+        Product product = new Product();
+        product.setCode("010101");
+        return product;
     }
 }
