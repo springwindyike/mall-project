cfab95595bb7791f5ce5c47574b60d2f06d88f5f
yinlin-tc <yinlin@91taoci.cn>
2015-08-06 09:14:19 +0800

    测试修改Json

--- mall-api/src/main/java/com/ishare/mall/controller/IndexController.java
+++ mall-api/src/main/java/com/ishare/mall/controller/IndexController.java
@@ -1,9 +1,14 @@
 package com.ishare.mall.controller;
 
+import com.google.common.collect.Lists;
+import com.ishare.mall.core.model.product.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by YinLin on 2015/7/30.
@@ -13,9 +18,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/index")
 public class IndexController {
-    @RequestMapping(value="get",method = RequestMethod.GET)
+    @RequestMapping(value = "get", method = RequestMethod.GET)
     @ResponseBody
     public String get() {
        return "success";
+    }
+
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    @ResponseBody
+    public Product list() {
+        Product product = new Product();
+        product.setBasePrice(10.0f);
+        product.setCode("1001001000");
+        return product;
     }
 }
