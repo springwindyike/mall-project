8b310c197abc2796337cd2d45e7c296f4bd1c525
yinlin-tc <yinlin@91taoci.cn>
2015-08-11 16:44:02 +0800

    新增oauth验证，新加doc Api文档说明

---
+++ mall-api/src/main/java/com/ishare/mall/restful/oauth/AuthorizeController.java
@@ -0,0 +1,64 @@
+package com.ishare.mall.restful.oauth;
+
+import org.apache.oltu.oauth2.as.issuer.MD5Generator;
+import org.apache.oltu.oauth2.as.issuer.OAuthIssuerImpl;
+import org.apache.oltu.oauth2.as.request.OAuthAuthzRequest;
+import org.apache.oltu.oauth2.as.response.OAuthASResponse;
+import org.apache.oltu.oauth2.common.OAuth;
+import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+import org.apache.oltu.oauth2.common.message.OAuthResponse;
+import org.apache.oltu.oauth2.common.message.types.ResponseType;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Created by YinLin on 2015/8/11.
+ * Description : Oauth2 接口 token认证接口
+ * Version 1.0
+ */
+@Controller
+public class AuthorizeController {
+    public Object authorize(Model model, HttpServletRequest request) throws URISyntaxException {
+        try {
+            OAuthAuthzRequest oauthRequest = new OAuthAuthzRequest(request);
+            //生成授权码
+            String authorizationCode = null;
+            //responseType目前仅支持CODE，另外还有TOKEN
+            String responseType = oauthRequest.getParam(OAuth.OAUTH_RESPONSE_TYPE);
+            if (responseType.equals(ResponseType.CODE.toString())) {
+                OAuthIssuerImpl oauthIssuerImpl = new OAuthIssuerImpl(new MD5Generator());
+                authorizationCode = oauthIssuerImpl.authorizationCode();
+                //oAuthService.addAuthCode(authorizationCode, username);
+            }
+            //进行OAuth响应构建
+            OAuthASResponse.OAuthAuthorizationResponseBuilder builder =
+                    OAuthASResponse.authorizationResponse(request,
+                            HttpServletResponse.SC_FOUND);
+            //设置授权码
+            builder.setCode(authorizationCode);
+            //得到到客户端重定向地址
+            String redirectURI = oauthRequest.getParam(OAuth.OAUTH_REDIRECT_URI);
+
+            //构建响应
+            final OAuthResponse response = builder.location(redirectURI).buildQueryMessage();
+            //根据OAuthResponse返回ResponseEntity响应
+            HttpHeaders headers = new HttpHeaders();
+            headers.setLocation(new URI(response.getLocationUri()));
+            return new ResponseEntity(headers, HttpStatus.valueOf(response.getResponseStatus()));
+        } catch (OAuthSystemException e) {
+            e.printStackTrace();
+        } catch (OAuthProblemException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
