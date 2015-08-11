7b543819294bd429e39d0c6777940eea26cc6f96
yinlin-tc <yinlin@91taoci.cn>
2015-08-11 13:34:43 +0800

    调整order api接口，新增oauth2依赖

--- mall-api/src/main/java/com/ishare/mall/restful/OrderResource.java
+++ mall-api/src/main/java/com/ishare/mall/restful/OrderResource.java
@@ -4,6 +4,7 @@ package com.ishare.mall.restful;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by YinLin on 2015/7/30.
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Version 1.0
  */
 @Controller
-@RequestMapping("/order")
+@RequestMapping("/orders")
 public class OrderResource {
     /**
      * 添加订单
@@ -32,6 +33,8 @@ public class OrderResource {
     /**
      * 详细订单信息
      */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
     public void detail() {
 
     }
