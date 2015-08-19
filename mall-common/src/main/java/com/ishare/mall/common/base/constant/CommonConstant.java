96bc4d42a4482f298e2f748d20c499c83055a0b9
yinlin-tc <yinlin@91taoci.cn>
2015-08-19 09:07:06 +0800

    新增支付接口

---
+++ mall-common/src/main/java/com/ishare/mall/common/base/constant/CommonConstant.java
@@ -0,0 +1,13 @@
+package com.ishare.mall.common.base.constant;
+
+/**
+ * Created by YinLin on 2015/8/18.
+ * Description :
+ * Version 1.0
+ */
+public interface CommonConstant {
+    //字符集
+    interface CharSet {
+        String UTF8 = "utf-8";
+    }
+}
