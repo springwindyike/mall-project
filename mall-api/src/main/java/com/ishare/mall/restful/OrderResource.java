7859d721b46714e9303afd72e99647a2c1653be6
zhangzhaoxin <zhang@zhaoxin.me>
2015-08-21 09:34:07 +0800

    订单 验证accesstoken

--- mall-api/src/main/java/com/ishare/mall/restful/OrderResource.java
+++ mall-api/src/main/java/com/ishare/mall/restful/OrderResource.java
@@ -4,13 +4,24 @@ package com.ishare.mall.restful;
 import static com.ishare.mall.common.base.constant.ResourceConstant.PAGE.LIMIT;
 import static com.ishare.mall.common.base.constant.ResourceConstant.PAGE.OFFSET;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.oltu.oauth2.common.OAuth;
+import org.apache.oltu.oauth2.common.error.OAuthError;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
+import org.apache.oltu.oauth2.common.message.OAuthResponse;
+import org.apache.oltu.oauth2.rs.response.OAuthRSResponse;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import java.util.List;
-
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +29,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ishare.mall.core.model.order.Order;
+import com.ishare.mall.core.service.oauth.OAuthService;
 import com.ishare.mall.core.service.order.OrderService;
 
 /**
@@ -31,6 +43,8 @@ public class OrderResource {
 	
 	@Autowired
 	private OrderService orderService;
+	@Autowired
+	private OAuthService oAuthService;
     /**
      * 添加订单
      */
@@ -53,10 +67,20 @@ public class OrderResource {
 	 * @param limit 分页size
 	 * @return 返回Order
      */
-	@RequestMapping(value = "/offset/{offset}/limit/{limit}", method = RequestMethod.GET)
+	@RequestMapping(value = "/offset/{offset}/limit/{limit}/accessToken/{accessToken}", method = RequestMethod.GET)
 	@ResponseBody
-	public Page<Order> list(@NotEmpty @PathVariable(OFFSET)Integer offset, @NotEmpty @PathVariable(LIMIT)Integer limit) {
-		PageRequest pageRequest = new PageRequest(offset - 1 < 0 ? 0 : offset - 1, limit <= 0 ? 15 : limit, Sort.Direction.DESC, "id");
+	public Object list(@NotEmpty @PathVariable(OFFSET)Integer offset, @NotEmpty @PathVariable(LIMIT)Integer limit,  @NotEmpty @PathVariable("accessToken") String accessToken) throws OAuthSystemException {
+		PageRequest pageRequest = new PageRequest(offset - 1 < 0 ? 0 : offset - 1, limit <= 0 ? 15 : limit, Sort.Direction.DESC, "orderId");
+		if (!oAuthService.checkAccessToken(accessToken)) {  
+			// 如果不存在/过期了，返回未验证错误，需重新验证  
+			OAuthResponse response = OAuthRSResponse  
+			        .errorResponse(HttpServletResponse.SC_UNAUTHORIZED)  
+			        .setError(OAuthError.ResourceResponse.INVALID_TOKEN)  
+			        .buildHeaderMessage();
+	    
+			return response;  
+		}  
+		//用findOne立即加载实体对象
 		Page<Order> result = orderService.search(null, pageRequest);
 		return result;
     }
@@ -66,9 +90,18 @@ public class OrderResource {
   * @param id
   * @return Order 返回的数据对象
 	 */
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "/{id}/accessToken/{accessToken}", method = RequestMethod.GET)
 	@ResponseBody
-	public Order detail(@NotEmpty @PathVariable("id") String id) {
+	public Object detail(@NotEmpty @PathVariable("id") String id, @NotEmpty @PathVariable("accessToken") String accessToken) throws OAuthSystemException {
+		if (!oAuthService.checkAccessToken(accessToken)) {  
+			// 如果不存在/过期了，返回未验证错误，需重新验证  
+			OAuthResponse response = OAuthRSResponse  
+			        .errorResponse(HttpServletResponse.SC_UNAUTHORIZED)  
+			        .setError(OAuthError.ResourceResponse.INVALID_TOKEN)  
+			        .buildHeaderMessage();
+	    
+			return response;  
+		}  
 		//用findOne立即加载实体对象
 		Order order = orderService.findOne(id);
 		return order;
