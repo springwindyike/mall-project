d69463e9bad62891d6ed8cbb2d68c419f1bdde8b
yinlin-tc <yinlin@91taoci.cn>
2015-08-24 14:29:47 +0800

    新增支付接口

---
+++ mall-api/src/main/java/com/ishare/mall/restful/PaymentResource.java
@@ -0,0 +1,12 @@
+package com.ishare.mall.restful;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by YinLin on 2015/8/24.
+ * Description :
+ * Version 1.0
+ */
+@Controller("/pay")
+public class PaymentResource {
+}
