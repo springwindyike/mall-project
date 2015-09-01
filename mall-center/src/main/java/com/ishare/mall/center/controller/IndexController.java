6e68de8584b1048753a8618df68e11e7304f330b
yinlin-tc <yinlin@91taoci.cn>
2015-09-01 17:53:07 +0800

    拆解项目结构，利于中间件

--- mall-center/src/main/java/com/ishare/mall/center/controller/IndexController.java
+++ mall-center/src/main/java/com/ishare/mall/center/controller/IndexController.java
@@ -1,5 +1,10 @@
 package com.ishare.mall.center.controller;
 
+import com.ishare.mall.center.controller.base.BaseController;
+import com.ishare.mall.core.form.center.login.LoginForm;
+import com.ishare.mall.core.service.information.ChannelService;
+import com.ishare.mall.core.service.member.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +15,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * Description :
  * Version 1.0
  */
-@Controller("/index")
-public class IndexController {
+@Controller
+public class IndexController extends BaseController {
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private ChannelService channelService;
     @RequestMapping("/res")
     @ResponseBody
     public Object result(Model m) {
         return m;
+    }
+    @RequestMapping(value = "/index")
+    public String index(){
+        return "index/center";
+    }
+
+    @RequestMapping(value = "/login")
+    public String login(LoginForm loginForm) {
+
+        return "index/center";
     }
 }
