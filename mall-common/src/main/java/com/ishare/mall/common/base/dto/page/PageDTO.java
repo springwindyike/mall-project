f5894ec4929a040d1dd0e751d35bae0dd50627bb
yinlin-tc <yinlin@91taoci.cn>
2015-09-08 12:29:45 +0800

    修改ueditor配置路径，IDEA和tomcat都能用

---
+++ mall-common/src/main/java/com/ishare/mall/common/base/dto/page/PageDTO.java
@@ -0,0 +1,16 @@
+package com.ishare.mall.common.base.dto.page;
+
+import org.codehaus.jackson.annotate.JsonAutoDetect;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by YinLin on 2015/9/7.
+ * Description :
+ * Version 1.0
+ */
+@XmlRootElement
+@JsonAutoDetect
+public class PageDTO {
+
+}
