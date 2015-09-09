4dfe80aab60a48bdb8b9ea8dc6d6914f35a32d74
yinlin-tc <yinlin@91taoci.cn>
2015-09-09 14:12:07 +0800

    自定义shiro

---
+++ mall-center/src/main/java/com/ishare/mall/center/shiro/exception/IncorrectCaptchaException.java
@@ -0,0 +1,27 @@
+package com.ishare.mall.center.shiro.exception;
+
+import org.apache.shiro.authc.AuthenticationException;
+
+/**
+ * Created by YinLin on 2015/9/9.
+ * Description : 自定义验证码错误异常
+ * Version 1.0
+ */
+public class IncorrectCaptchaException extends AuthenticationException {
+
+    public IncorrectCaptchaException() {
+        super();
+    }
+
+    public IncorrectCaptchaException(String message) {
+        super(message);
+    }
+
+    public IncorrectCaptchaException(Throwable cause) {
+        super(cause);
+    }
+
+    public IncorrectCaptchaException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
