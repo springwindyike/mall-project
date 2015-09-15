9acaffffdf4f597968d595a68eb349d19581ca0b
wanghao-tc <wanghao@91taoci.cn>
2015-09-15 18:40:55 +0800

    修改api product

--- mall-common/src/main/java/com/ishare/mall/common/base/dto/product/ProductListDTO.java
+++ mall-common/src/main/java/com/ishare/mall/common/base/dto/product/ProductListDTO.java
@@ -1,12 +1,19 @@
 package com.ishare.mall.common.base.dto.product;
 
+import com.ishare.mall.common.base.dto.page.PageDTO;
 import com.ishare.mall.common.base.object.BaseObject;
+import org.codehaus.jackson.annotate.JsonAutoDetect;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Map;
 
 /**
  * Created by YinLin on 2015/8/7.
  * Description: 商品列表API DTO 只要是查询或者List全用这个DTO
  * Version 1.0
  */
+@XmlRootElement
+@JsonAutoDetect
 public class ProductListDTO implements BaseObject {
     //商品ID
     private Integer id;
@@ -16,7 +23,10 @@ public class ProductListDTO implements BaseObject {
     private Float sellPrice;
     //默认样式图片
     private String defaultImageUrl;
-
+    private Integer limit;
+    private Integer offset;
+    private PageDTO pageDTO;
+    private Map<String,Object> map;
     public Integer getId() {
         return id;
     }
@@ -47,5 +57,37 @@ public class ProductListDTO implements BaseObject {
 
     public void setDefaultImageUrl(String defaultImageUrl) {
         this.defaultImageUrl = defaultImageUrl;
+    }
+
+    public Integer getOffset() {
+        return offset;
+    }
+
+    public void setOffset(Integer offset) {
+        this.offset = offset;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public PageDTO getPageDTO() {
+        return pageDTO;
+    }
+
+    public void setPageDTO(PageDTO pageDTO) {
+        this.pageDTO = pageDTO;
+    }
+
+    public Map<String, Object> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Object> map) {
+        this.map = map;
     }
 }
