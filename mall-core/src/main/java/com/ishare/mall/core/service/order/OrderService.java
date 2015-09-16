6358663ba70c4fa7591509e6cefdb1d782ed7ef4
yinlin-tc <yinlin@91taoci.cn>
2015-09-16 13:58:39 +0800

    修改配置

--- mall-core/src/main/java/com/ishare/mall/core/service/order/OrderService.java
+++ mall-core/src/main/java/com/ishare/mall/core/service/order/OrderService.java
@@ -2,13 +2,11 @@ package com.ishare.mall.core.service.order;
 
 import com.ishare.mall.common.base.dto.order.ExchangeDTO;
 import com.ishare.mall.core.model.order.Order;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
-import com.ishare.mall.core.model.order.Order;
+import java.util.List;
+import java.util.Map;
 
 public interface OrderService {
 	
@@ -28,6 +26,11 @@ public interface OrderService {
 	 */
 	Order payComplete(String orderId);
 
+	/**
+	 * 创建订单
+	 * @param exchangeDTO
+	 * @return
+	 */
 	Order create(ExchangeDTO exchangeDTO);
 	
 	/**
