e73a92d108b973e21dc112b73da69ec8579ed063
yinlin-tc <yinlin@91taoci.cn>
2015-09-22 11:06:14 +0800

    增加异常处理

---
+++ mall-common/src/main/java/com/ishare/mall/common/base/constant/IShareErrorConstant.java
@@ -0,0 +1,16 @@
+package com.ishare.mall.common.base.constant;
+
+/**
+ * Created by YinLin on 2015/9/22.
+ * Description :
+ * Version 1.0
+ */
+public interface IShareErrorConstant {
+    public static final String ERROR_NULL = "0";
+    public static final String ERROR_DIV_0 = "7";
+    public static final String ERROR_VALUE = "15";
+    public static final String ERROR_REF = "23";
+    public static final String ERROR_NAME = "29";
+    public static final String ERROR_NUM = "36";
+    public static final String ERROR_NA = "42";
+}
