8e92b0cddc6165bb0c8124bb5efa99888f6d296e
wanghao-tc <wanghao@91taoci.cn>
2015-09-23 17:01:48 +0800

    revert 到最新版本

--- mall-common/src/main/java/com/ishare/mall/common/base/general/Response.java
+++ mall-common/src/main/java/com/ishare/mall/common/base/general/Response.java
@@ -1,10 +1,13 @@
 package com.ishare.mall.common.base.general;
 
-import com.ishare.mall.common.base.dto.generic.GenericDTO;
+import com.ishare.mall.common.base.dto.test.TestDTO;
 import org.codehaus.jackson.annotate.JsonAutoDetect;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.util.Map;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import java.io.Serializable;
 
 /**
  * Created by YinLin on 2015/9/22.
@@ -13,18 +16,26 @@ import java.util.Map;
  */
 
 @XmlRootElement
+@XmlAccessorType(value = XmlAccessType.PROPERTY)
+@XmlSeeAlso(
+        {
+                TestDTO.class,
+        }
+)
 @JsonAutoDetect
-public class Response<T extends GenericDTO> extends GenericDTO {
+public class Response<T> implements Serializable {
+
+    private static final long serialVersionUID = 3886269150595934287L;
+
     //错误码
     private Integer code;
     //是否操作成功
     private boolean success = Status.SUCCESS;
     //数据返回数据对象
-    private Object data;
+    private T data;
     //错误消息
     private String message;
 
-    private Map<String,T> map;
     public Integer getCode() {
         return code;
     }
@@ -41,11 +52,11 @@ public class Response<T extends GenericDTO> extends GenericDTO {
         this.success = success;
     }
 
-    public Object getData() {
+    public T getData() {
         return data;
     }
 
-    public void setData(Object data) {
+    public void setData(T data) {
         this.data = data;
     }
 
@@ -55,14 +66,6 @@ public class Response<T extends GenericDTO> extends GenericDTO {
 
     public void setMessage(String message) {
         this.message = message;
-    }
-
-    public Map getMap() {
-        return map;
-    }
-
-    public void setMap(Map map) {
-        this.map = map;
     }
 
     public interface Status {
