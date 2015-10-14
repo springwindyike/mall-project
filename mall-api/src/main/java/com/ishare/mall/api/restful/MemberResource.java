0ed9bc65df4f303c80da45256c904a5237432e74
yinlin-tc <yinlin@91taoci.cn>
2015-10-14 15:49:38 +0800

    修改API用户订单列表

--- mall-api/src/main/java/com/ishare/mall/api/restful/MemberResource.java
+++ mall-api/src/main/java/com/ishare/mall/api/restful/MemberResource.java
@@ -1,19 +1,7 @@
 package com.ishare.mall.api.restful;
 
-import com.ishare.mall.common.base.dto.member.MemberDetailDTO;
-import org.apache.oltu.oauth2.common.error.OAuthError;
-import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
-import org.apache.oltu.oauth2.common.message.OAuthResponse;
-import org.apache.oltu.oauth2.rs.response.OAuthRSResponse;
-import org.hibernate.validator.constraints.NotEmpty;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by YinLin on 2015/8/5.
@@ -23,44 +11,5 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 @RequestMapping("/members")
 public class MemberResource {
-	
-//	@Autowired
-//	private OAuthService oAuthService;
-//
-//	@Autowired
-//	private MemberService memberService;
-//
-//    @RequestMapping(value = "/create", method = RequestMethod.POST)
-//    @ResponseBody
-//    public Object create() {
-//        return null;
-//    }
-//
-//    /**
-//     * 通过accessToken获取到ID查询出memeber信息
-//  * @param accessToken
-//  * @return Member 返回的数据对象
-//	 */
-//	@RequestMapping(value = "/accessToken/{accessToken}", method = RequestMethod.GET)
-//	@ResponseBody
-//    public Object detail(@NotEmpty @PathVariable("accessToken") String accessToken) throws OAuthSystemException {
-//		if (!oAuthService.checkAccessToken(accessToken)) {
-//			// 如果不存在/过期了，返回未验证错误，需重新验证
-//			OAuthResponse response = OAuthRSResponse
-//			        .errorResponse(HttpServletResponse.SC_UNAUTHORIZED)
-//			        .setError(OAuthError.ResourceResponse.INVALID_TOKEN)
-//			        .buildHeaderMessage();
-//
-//			return response;
-//		}
-//		String account = oAuthService.getAccountByAccessToken(accessToken);
-//		Member member = memberService.findByAccount(account);
-//		return MapperUtils.map(member, MemberDetailDTO.class);
-//	}
-//
-//    public Member update() {
-//        return null;
-//    }
-
 
 }
