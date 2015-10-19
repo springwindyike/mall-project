55af1427832b569669ddf62913cf8b51bac22fe3
yinlin-tc <yinlin@91taoci.cn>
2015-10-19 09:11:41 +0800

    修改日志和后台管理

---
+++ mall-core/src/main/java/com/ishare/mall/core/model/manage/ManageUser.java
@@ -0,0 +1,14 @@
+package com.ishare.mall.core.model.manage;
+
+import com.ishare.mall.core.model.base.BaseEntity;
+
+/**
+ * Created by YinLin on 2015/10/16.
+ * Description :
+ * Version 1.0
+ */
+public class ManageUser extends BaseEntity {
+    private Integer id;
+    private String username;
+    private String password;
+}
