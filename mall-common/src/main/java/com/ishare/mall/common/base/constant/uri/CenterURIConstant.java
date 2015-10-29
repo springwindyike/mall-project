c803357dcd536fa9eaff0b0ca022f68002175ff5
liaochenglei <liaochenglei@51ishare.com>
2015-10-29 19:03:46 +0800

    update

--- mall-common/src/main/java/com/ishare/mall/common/base/constant/uri/CenterURIConstant.java
+++ mall-common/src/main/java/com/ishare/mall/common/base/constant/uri/CenterURIConstant.java
@@ -45,6 +45,7 @@ public interface CenterURIConstant {
 	  String REQUEST_MAPPING_FIND_ALL_BRAND="/allBrand";
 	  String REQUEST_MAPPING_DELETE_BY_ID ="/del/{id}";
 	  String REQUEST_MAPPING_UPDATE_BY_ID ="/update/{id}";
+	  String REQUEST_MAPPING_ADD ="/add";
   }
 	interface Member {
 		String REQUEST_MAPPING = "/member";
