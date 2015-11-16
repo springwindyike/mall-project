6e2bb842c39ae4eee187875067eb8715bc8955c9
yinlin-tc <yinlin@91taoci.cn>
2015-11-16 13:33:02 +0800

    修改Pom

--- mall-image/src/main/java/TestA.java
+++ mall-image/src/main/java/TestA.java
@@ -1,14 +1,10 @@
-package com.mall.image;
-
 /**
  * Created by YinLin on 2015/11/16.
  * Description :
  * Version 1.0
  */
-public class Test {
+public class TestA {
     public static void main(String[] args) {
-        int i = 0;
-        int a = 1;
-        System.out.println(a + "");
+        System.out.println("hello");
     }
 }
